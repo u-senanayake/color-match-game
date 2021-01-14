f7abd4324484e2003947cbf70e9646309b06d058
u-senanayake <mailtoudayanga@gmail.com>
2021-01-14 14:49:51 +0900

    Initial project

---
+++ app/src/main/java/senanayake/udayanga/com/colormatchgame/ui/slideshow/SlideshowViewModel.java
@@ -0,0 +1,19 @@
+package senanayake.udayanga.com.colormatchgame.ui.slideshow;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SlideshowViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public SlideshowViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is slideshow fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
